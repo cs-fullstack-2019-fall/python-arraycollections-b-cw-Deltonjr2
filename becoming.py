551788e5b7e74fa4730d5c051f80059ca90486a8
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-27 12:49:46 -0500

    list

---
+++ becoming.py
@@ -0,0 +1,83 @@
+#
+# python-arrayCollections_b-cw
+# Problem 1:
+#
+# Create a function with the variable below. After you create the variable do the instructions below that.
+#
+# arrayForProblem2 = ["Kenn", "Kevin", "Erin", "Meka"]
+# a) Print the 3rd element of the numberList.
+#
+#     b) Print the size of the array
+#
+# c) Delete the second element.
+#
+#     d) Print the 3rd element.
+#
+#     Problem 2:
+#
+# We will keep having this problem until EVERYONE gets it right without help
+#
+# Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
+#
+# Problem 3:
+#
+#     Create a function that contains a collection of information for the following. After you create the collection do the instructions below that.
+#
+# Jonathan/John
+# Michael/Mike
+# William/Bill
+# Robert/Rob
+# a) Print the collection
+#
+# b) Print William's nickname
+#
+# Problem 4:
+#
+#     Create an array of 5 numbers. Using a loop, print the elements in the array reverse order. Do not use a function
+#
+# Problem 5:
+#
+# Create a function that will have a hard coded array then ask the user for a number. Use the userInput to state how many numbers in an array are higher, lower, or equal to it.
+#
+# //1  Create a function with the variable below. After you create the variable do the instructions below that.
+
+    def problem1():
+    def arrayForProblem2():
+
+    def arrayForProblem2():
+        listNames = [ "Kenn", "Kevin", "Erin", "Meka"]
+# a.Print the 3rd element of the numberList.
+        print(listNames[3])
+        listNames[3]= "Meka"
+
+# //b. Print the size of the array
+        listNames = [4]
+        print(listNames)
+
+# c) DeletelistNames [4] the second element.
+        listNames.append("Kevin")
+        print(listNames)
+
+# d) Print the 3rd element.
+        print [3] ="Meka"
+
+# 2.Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
+#
+
+def problem2():
+    userInput = ""
+    while(userInput != 'q'):
+        userInput = input("Try again")
+
+# *******************************Teacher Advised not to work this problem*****************************
+# 3.  Create a function that contains a collection of information for the following. After you create the collection do the instructions below that.
+
+
+# 4. Create an array of 5 numbers. Using a loop, print the elements in the array reverse order. Do not use a function
+CreateArray= [1,2,3,4,5]
+
+for everyNum in createArray:
+    if createArray < 6:
+        print(f"The numbers 1,2,3,4,5 ")
+
+        print(f"The numbers 5,4,3,2,1 ")
